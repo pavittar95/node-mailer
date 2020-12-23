0feb1f66a3c9a6575d4dffaa5a82bad68a8af05e
Pavittar Singh <singh.pavittar95@icloud.com>
2020-12-23 23:25:08 +0530

    readme updated

--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ To allow sending mail using your gmail id. Follow the following steps
 
 1. Go to you gmail -> open settings -> Forwarding and POP/IMAP -> enable IMAP in Status: IMAP is enabled section -> save changes
 2. Also enable the captcha at with the same account. https://accounts.google.com/DisplayUnlockCaptcha
+3. Search enable less secure apps and enable the app
--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const nodemailer = require('nodemailer');
-const CONST = require('../constants');
+const nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    
+  }
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Server running');
+router.get("/", function (req, res, next) {
+  res.send("Server running");
 });
-router.post('/sendMail', function(req, res, next) {
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: CONST.auth,
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
+router.post("/sendMail", function (req, res, next) {
   transporter.sendMail(
     {
-      from: `<${req.body.mail}>`,
-      to: CONST.mail,
-      subject: `You have new query from ${req.body.name}`,
-      text: 'You have new query',
-      html: `<p>${req.body.msg}</p>`
+      from: 'sgc.architect@gmail.com',
+      to: 'pavitar@supertal.io',
+      subject: "Paypal Hooks",
+      text: "Paypal Hooks",
+      html: JSON.stringify(req.body),
     },
     (error, info) => {
       if (error) {
+        console.log(info);
+        res.send("Not able to send email at the moment.");
         return console.log(error);
       }
-    }
-  );
-  transporter.sendMail(
-    {
-      from: '"SGC Architect"',
-      to: req.body.mail,
-      subject: 'SGC Architect',
-      text: 'SGC Architect',
-      html: `<div>Hi ${req.body.name}
-      <p>We have received your message. we will contact in sort period of time.</p>
-      <p>Thank you</p>
-      <p>Sgc Architect</p>
-      </div>`
-    },
-    (error, info) => {
-      if (error) {
-        res.send('Not able to send email at the moment.');
-        return console.log(error);
-      }
-      res.send('Mail send');
+      res.send("Mail send");
     }
   );
 });
